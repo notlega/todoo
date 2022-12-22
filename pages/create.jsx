c5a9a55b5ca0e1376166f2feb3506d891c28e110
huishilhs <huishilhs@gmail.com>
2022-12-23 01:39:32 +0800

    delete error

--- pages/create.jsx
+++
@@ -1,9 +0,0 @@
-        onSubmit={async () =>
-         await mutate({
-            title: title,
-            content: content,
-            start_date_time: moment(start).format(),
-            end_date_time: moment(end).format(),
-            id: id,
-          })
-        }
